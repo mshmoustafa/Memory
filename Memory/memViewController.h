80cf575918c0038e6664cdd4968acd6eb5af34fb
Muhammad-Sharif Moustafa <sharif@Muhammad-Sharifs-MacBook-Pro.local>
2014-02-03 23:40:20 -0500

    added more cards added alert when you win, as well as option to play again added creategame method to make it possible to make a new game at any time in the app

--- Memory/memViewController.h
+++ Memory/memViewController.h
@@ -8,25 +8,55 @@
 
 #import <UIKit/UIKit.h>
 
-@interface memViewController : UIViewController
+@interface memViewController : UIViewController <UIAlertViewDelegate>
 {
+    int score;
+    int moves;
+    
     NSArray *buttons;
     
     NSArray *cards;
-    NSNumber *firstCard;
+    UIButton *firstCard;
 //    NSString *secondCard;
 }
 
-- (void)whichCardClicked:(NSNumber *)cardNumber;
+- (void)createGame;
+
+- (void)initializeRandomPairs:(int)numberOfMatches;
+
+- (void)setScore:(int)scoreToSet;
+
+- (void)whichCardClicked:(UIButton *)cardNumber;
+
 - (BOOL)checkIf:(int)card1 Matches:(int)card2;
 
-@property (weak, nonatomic) IBOutlet UIButton *buttonOne;
-- (IBAction)buttonOnePress:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *buttonTwo;
-- (IBAction)buttonTwoPress:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *buttonThree;
-- (IBAction)buttonThreePress:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *buttonFour;
-- (IBAction)buttonFourPress:(id)sender;
+
+- (IBAction)cardPress:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+@property (weak, nonatomic) IBOutlet UIButton *button10;
+@property (weak, nonatomic) IBOutlet UIButton *button11;
+@property (weak, nonatomic) IBOutlet UIButton *button12;
+@property (weak, nonatomic) IBOutlet UIButton *button13;
+@property (weak, nonatomic) IBOutlet UIButton *button14;
+@property (weak, nonatomic) IBOutlet UIButton *button15;
+@property (weak, nonatomic) IBOutlet UIButton *button16;
+@property (weak, nonatomic) IBOutlet UIButton *button17;
+@property (weak, nonatomic) IBOutlet UIButton *button18;
+@property (weak, nonatomic) IBOutlet UIButton *button19;
+@property (weak, nonatomic) IBOutlet UIButton *button20;
+@property (weak, nonatomic) IBOutlet UIButton *button21;
+@property (weak, nonatomic) IBOutlet UIButton *button22;
+@property (weak, nonatomic) IBOutlet UIButton *button23;
+@property (weak, nonatomic) IBOutlet UIButton *button24;
 
 @end
--- Memory/memViewController.m
+++ Memory/memViewController.m
@@ -17,34 +17,73 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    [self createGame];
     buttons = [NSArray arrayWithObjects:
-               [self buttonOne],
-               [self buttonTwo],
-               [self buttonThree],
-               [self buttonFour],
+               [self button1],
+               [self button2],
+               [self button3],
+               [self button4],
+               [self button5],
+               [self button6],
+               [self button7],
+               [self button8],
+               [self button9],
+               [self button10],
+               [self button11],
+               [self button12],
+               [self button13],
+               [self button14],
+               [self button15],
+               [self button16],
+               [self button17],
+               [self button18],
+               [self button19],
+               [self button20],
+               [self button21],
+               [self button22],
+               [self button23],
+               [self button24],
                Nil
-                ];
+               ];
     
     //1 is yellow, 2 is red
     //@1 is short for [NSNumber numberWithInt:1]
-//    cards = @{@"1" : @1,
-//              @"2" : @1,
-//              @"3" : @2,
-//              @"4" : @2,
-//              };
-    cards = [NSArray arrayWithObjects:
-             @1,
-             @1,
-             @2,
-             @2,
-             Nil];
+    //    cards = @{@"1" : @1,
+    //              @"2" : @1,
+    //              @"3" : @2,
+    //              @"4" : @2,
+    //              };
     
-    firstCard = @-1;
-//    secondCard = @"0";
     
-    [[self buttonOne] setBackgroundColor:Nil];
+//    cards = [NSArray arrayWithObjects:
+//             [UIColor redColor],
+//             [UIColor redColor],
+//             [UIColor yellowColor],
+//             [UIColor yellowColor],
+//             Nil];
+    
+    //    secondCard = @"0";
+    
+}
 
+//implemented UIAlertViewDelegate methods
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        [self createGame];
+    }
+}
+
+- (void)createGame
+{
+    for (UIButton *button in buttons) {
+        [button setBackgroundColor:[UIColor clearColor]];
+        [button setEnabled:YES];
+        [button setAlpha:(CGFloat)1.0];
+    }
+    [self initializeRandomPairs:12];
+    [self setScore:0];
+    moves = 0;
+    firstCard = nil;
 }
 
 - (void)didReceiveMemoryWarning
@@ -53,36 +92,116 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)whichCardClicked:(NSNumber *)cardNumber
+- (void)initializeRandomPairs:(int)numberOfMatches
 {
-    if ([firstCard  isEqual: @-1]) {
+    NSMutableArray *randomPairs = [NSMutableArray arrayWithCapacity:(numberOfMatches * 2)];
+    int numberRed = numberOfMatches / 2;
+    int numberYellow = numberOfMatches / 2;
+    int numberBlue = numberOfMatches / 2;
+    int numberGreen = numberOfMatches / 2;
+    
+//    for (int i = 0; i < (numberOfMatches * 2); i++) {
+        while ((numberRed != 0) || (numberYellow != 0) || (numberBlue != 0) || (numberGreen != 0)) {
+            int random = (arc4random() % 4 + 1);
+            NSLog(@"Random number: %d", random);
+            if (random == 1 && numberRed != 0) {
+                [randomPairs addObject:[UIColor redColor]];
+                numberRed--;
+            } else if (random == 2 && numberYellow != 0) {
+                [randomPairs addObject:[UIColor yellowColor]];
+                numberYellow--;
+            } else if (random == 3 && numberBlue != 0) {
+                [randomPairs addObject:[UIColor blueColor]];
+                numberBlue--;
+            } else if (random == 4 && numberGreen != 0) {
+                [randomPairs addObject:[UIColor greenColor]];
+                numberGreen--;
+            }
+        }
+//        if (numberRed == 0 && numberYellow == 0 && numberBlue == 0 && numberGreen == 0) {
+//            //this shouldn't happen, do something about it
+//        } else if (numberRed == 0) {
+//            [randomPairs addObject:[UIColor yellowColor]];
+//            numberYellow--;
+//        } else if (numberYellow == 0) {
+//            [randomPairs addObject:[UIColor redColor]];
+//            numberRed--;
+//        } else {
+//            if (random == 1) {
+//                [randomPairs addObject:[UIColor redColor]];
+//                numberRed--;
+//            } else if (random == 2) {
+//                [randomPairs addObject:[UIColor yellowColor]];
+//                numberYellow--;
+//            }
+//        }
+//    }
+    
+    cards = [randomPairs copy];
+}
+
+- (void)setScore:(NSInteger)scoreToSet
+{
+    score = scoreToSet;
+    
+    if (score == cards.count/2) {
+        [self.scoreLabel setText:[NSString stringWithFormat:@"You win!  %d matches out of %d",scoreToSet,(cards.count)/2]];
+        UIAlertView *win = [[UIAlertView alloc]
+                            initWithTitle:@"You win!"
+                            message:[NSString stringWithFormat:@"You won in %d moves (the least number of moves to win is 12.)",moves]
+                            delegate:self
+                            cancelButtonTitle:@"OK"
+                            otherButtonTitles:@"New game", nil];
+        [win show];
+    } else {
+        [self.scoreLabel setText:[NSString stringWithFormat:@"%d matches out of %d",scoreToSet,(cards.count)/2]];
+    }
+}
+
+- (void)whichCardClicked:(UIButton *)cardNumber
+{
+    if (firstCard == nil) {
+        
         firstCard = cardNumber;
-        NSLog(@"First card is %@.", firstCard);
-    } else if ([firstCard isEqual: cardNumber]) {
+        NSLog(@"First card is %d.", [firstCard tag]);
+        
+    } else if (firstCard == cardNumber) {
+        
         //do nothing - this means you clicked the first card again.
         NSLog(@"You clicked on the same card!");
+        
     } else {
-        NSLog(@"Second card is %@.", cardNumber);
-        if ([self checkIf:(int)[firstCard integerValue] Matches:(int)[cardNumber integerValue]]) {
+        
+        NSLog(@"Second card is %d.", [cardNumber tag]);
+        
+        if ([self checkIf:[firstCard tag] Matches:[cardNumber tag]] == YES) {
+            
             NSLog(@"Match!");
-            [[buttons objectAtIndex:[firstCard integerValue]] setEnabled:NO];
-            [[buttons objectAtIndex:[firstCard integerValue]] setAlpha:(CGFloat)0.5];
-            [[buttons objectAtIndex:[cardNumber integerValue]] setEnabled:NO];
-            [[buttons objectAtIndex:[cardNumber integerValue]] setAlpha:(CGFloat)0.5];
             
-            firstCard = @-1;
+            [self setScore:(++score)];
+            moves++;
+            
+            [firstCard setEnabled:NO];
+            [firstCard setAlpha:(CGFloat)0.5];
+            [cardNumber setEnabled:NO];
+            [cardNumber setAlpha:(CGFloat)0.5];
+            
+            firstCard = nil;
         } else {
+            
             NSLog(@"No match...");
+            
+            moves++;
+            
             [UIView animateWithDuration:1 animations:^{
-                ((UIButton *)[buttons objectAtIndex:[firstCard integerValue]]).backgroundColor = [UIColor clearColor];
-            }];
-            [UIView animateWithDuration:1 animations:^{
-                ((UIButton *)[buttons objectAtIndex:[cardNumber integerValue]]).backgroundColor = [UIColor clearColor];
+                firstCard.backgroundColor = [UIColor clearColor];
             }];
             
-            [self reloadInputViews];
+            [UIView animateWithDuration:1 animations:^{
+                cardNumber.backgroundColor = [UIColor clearColor];
+            }];
             
-            firstCard = @-1;
+            firstCard = nil;
         }
     }
 }
@@ -91,8 +210,8 @@
 {
     BOOL isMatch = NO;
     
-//    NSNumber *color1 = [cards objectAtIndex:[card1 intValue]];
-
+    //    NSNumber *color1 = [cards objectAtIndex:[card1 intValue]];
+    
     if ([cards[card1] isEqual:cards[card2]]) {
         isMatch = YES;
     }
@@ -100,35 +219,13 @@
     return isMatch;
 }
 
-- (IBAction)buttonOnePress:(id)sender {
-//    [[self buttonOne] setBackgroundColor:(UIColor *)[UIColor redColor]];
+- (IBAction)cardPress:(id)sender {
+    //    [[self buttonOne] setBackgroundColor:(UIColor *)[UIColor redColor]];
     [UIView animateWithDuration:0.15 animations:^{
-        [self buttonOne].backgroundColor = [UIColor redColor];
+        ((UIButton *) sender).backgroundColor = cards[((UIButton *) sender).tag];
     }];
-    [self whichCardClicked:@0];
+    
+    [self whichCardClicked:(UIButton *)sender];
 }
 
-- (IBAction)buttonTwoPress:(id)sender {
-//    [[self buttonTwo] setBackgroundColor:(UIColor *)[UIColor redColor]];
-    [UIView animateWithDuration:0.15 animations:^{
-        [self buttonTwo].backgroundColor = [UIColor redColor];
-    }];
-    [self whichCardClicked:@1];
-}
-
-- (IBAction)buttonThreePress:(id)sender {
-//    [[self buttonThree] setBackgroundColor:(UIColor *)[UIColor yellowColor]];
-    [UIView animateWithDuration:0.15 animations:^{
-        [self buttonThree].backgroundColor = [UIColor yellowColor];
-    }];
-    [self whichCardClicked:@2];
-}
-
-- (IBAction)buttonFourPress:(id)sender {
-//    [[self buttonFour] setBackgroundColor:(UIColor *)[UIColor yellowColor]];
-    [UIView animateWithDuration:0.15 animations:^{
-        [self buttonFour].backgroundColor = [UIColor yellowColor];
-    }];
-    [self whichCardClicked:@3];
-}
 @end
